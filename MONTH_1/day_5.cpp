2dc4b7be05e1d449a398b7e1212683893424f4be
Daksh Gopani <dakshgopani0123@gmail.com>
2026-01-05 21:30:25 +0530

    day 5 code

---
+++ MONTH_1/day_5.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-the-pivot-integer/?envType=problem-list-v2&envId=maths-m1-arithmetic-basic-reasoning
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        
+        int S = n * (n + 1) / 2;
+
+        int x = sqrt(S);
+
+        if (x * x == S) return x;
+        return -1;
+    }
+};
+
