1ba38bcabb326b14da8d69c933e03cc9c1c00cb4
Daksh Gopani <dakshgopani0123@gmail.com>
2026-01-28 02:00:58 +0530

    day 28 code

---
+++ MONTH_1/day_28.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // target is to merge two lists in the sorted order
+        // so list1 head can be big sometimes else list2 head can be big sometimes
+        // this code uses recursive approach
+
+        // base case
+        if(list1 == NULL || list2 == NULL)
+            return list1 == NULL ? list2 : list1;
+        
+        // case 1 if head2 > head1
+        if(list1->val <= list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        // case 2
+        else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+        
+    }
+};
