e00b0ec0154c29a1c0e167367f17011032f01d4c
Daksh Gopani <dakshgopani0123@gmail.com>
2026-02-01 16:44:47 +0530

    day 32 code

---
+++ MONTH_2/day_1.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/split-a-string-in-balanced-strings/
+
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count = 0;
+        int balance = 0;
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'R') balance++;
+            else balance--;
+
+            if(balance == 0) count++;
+
+        }
+
+        return count;        
+    }
+};
