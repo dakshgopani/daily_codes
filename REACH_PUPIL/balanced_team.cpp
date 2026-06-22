6c0b0c92a63e88caa482f761636fcb083834ec74
Daksh Gopani <dakshgopani0123@gmail.com>
2026-06-23 01:40:16 +0530

    modified 22 june code

--- REACH_PUPIL/balanced_team.cpp
+++ REACH_PUPIL/balanced_team.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
     cin >> n;
 
@@ -10,13 +10,15 @@ int main(){
 
     for(int i = 0; i < n; i++)
         cin >> arr[i];
-    
+
+    sort(arr.begin(), arr.end());
+
     int l = 0, ans = 0;
 
-    for(int r = 0; r < n; r++){
-        if(arr[r] - arr[l] > 5)
+    for(int r = 0; r < n; r++) {
+        while(arr[r] - arr[l] > 5)
             l++;
-        
+
         ans = max(ans, r - l + 1);
     }
 
